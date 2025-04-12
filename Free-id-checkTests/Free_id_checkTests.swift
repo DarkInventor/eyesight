4757b710b11d99edb9533fb0e0a30741b0c6e3b7
DarkInventor <ktmehta25@gmail.com>
2025-04-12 03:03:50 -0400

    Initial Commit

---
+++ Free-id-checkTests/Free_id_checkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Free_id_checkTests.swift
+//  Free-id-checkTests
+//
+//  Created by Kathan Mehta on 2025-04-12.
+//
+
+import Testing
+@testable import Free_id_check
+
+struct Free_id_checkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Free-id-checkUITests/Free_id_checkUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Free_id_checkUITestsLaunchTests.swift
+//  Free-id-checkUITests
+//
+//  Created by Kathan Mehta on 2025-04-12.
+//
+
+import XCTest
+
+final class Free_id_checkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
